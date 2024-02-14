c82b25d9ef9b5344da8da73bbc524a9f8c090201
gxz <xizhuogao@foxmail.com>
2024-02-14 23:10:05 +0800

    文档的增删改查

--- src/main/java/com/gxz/pojo/book.java
+++ src/main/java/com/gxz/pojo/book.java
@@ -6,9 +6,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.MongoId;
-
-import java.math.BigInteger;
 
 @Data
 @NoArgsConstructor
